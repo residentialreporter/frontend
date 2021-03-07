6be98bd7f5658e3abc5a3c4fceb94729ae552efd
ri0t <riot@c-base.org>
2021-03-07 21:07:25 +0100

    temporarily removes activation call - it is WiP!

--- src/app/pages/activation/activation-page.component.ts
+++ src/app/pages/activation/activation-page.component.ts
@@ -25,11 +25,15 @@ export class ActivationPageComponent implements OnInit {
     async ngOnInit() {
         this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl || '/home';
 
+        /*
         this.activatedRoute.params
             .subscribe(params => {
                 console.log('Activating UUID:', params.uuid);
                 return this.userService.activate(params.uuid);
             });
+
+
+        */
 
         // if (await this.userService.checkAuthenticated()) {
         //    await this.router.navigate([this.returnUrl]);
